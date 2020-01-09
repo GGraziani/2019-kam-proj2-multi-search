10fa87cda8b5aa784ffe5923c7a9dc4b46ff1b14
gus <gustavo.graz@outlook.com>
2020-01-09 09:56:14 +0100

    Edit docs

--- utils/doc_utils.py
+++ utils/doc_utils.py
@@ -6,8 +6,6 @@ by Gustavo Graziani
 
 Commands:
 {onto_creator}
-{individ_creator}
-{find_bad_smells}
 
 
 TO SEE DETAILS ON EACH COMMAND, RUN
@@ -15,35 +13,16 @@ TO SEE DETAILS ON EACH COMMAND, RUN
 '''
 
 MODULE_DOCSTRINGS = {
-    'onto_creator': '''
-onto_creator:
-     Creates an ontology for Java entities.
+    'extract_data': '''
+extract_data:
+     Extract names of Python and C++ classes, methods, functions.
 
     Example usage:
-        $ python3 bad_smells.py onto_creator
+        $ python3 multi_search.py extract_data
 
     flags:
     -s <path-to-file> | --source <path-to-file>:
         The path to the "tree.py" file. Default is "PROJ_ROOT/lib/tree.py"
-''',
-    'individ_creator': '''
-individ_creator:
-    Populates the ontology (tree.owl) with instances of ClassDeclaration, MethodDeclaration, FieldDeclaration, 
-    Statement subclasses (e.g., IfStatement, WhileStatement, etc.) and FormalParameter.
-
-    Example usage:
-        $ python3 bad_smells.py individ_creator
-
-    flags:
-    -s <path-to-source> | --source <path-to-source>:
-        The path to a directory containing java files. Default is "PROJ_ROOT/lib/android-chess/app/src/main/java/jwtc/chess"
-''',
-    'find_bad_smells': '''
-find_bad_smells:
-    Runs Sparql queries to detect bad smells (Long Method, Large Class, Long Parameter List, Switch Statements, Data Class).
-
-    Example usage:
-        $ python3 bad_smells.py find_bad_smells
 '''
 }
 
