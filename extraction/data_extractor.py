1ba3538a7c9363963f0fcb9d079a4f157d9998ae
gus <gustavo.graz@outlook.com>
2020-01-10 02:13:25 +0100

    Add traverse and collect_data methods

--- extraction/data_extractor.py
+++ extraction/data_extractor.py
@@ -1,14 +1,26 @@
 import os
 import sys
 
+from utils.misc import traverse
+
+py_files = []
+clang_files = []
+
 
 def extract_data(file_path):
-    print(file_path)
+    traverse(file_path, r'\.py$|\.cc$', collect_files)
+
+    print(py_files)
+    print(clang_files)
+
+
+def collect_files(file):
+    py_files.append(file) if file.endswith('.py') else clang_files.append(file)
 
 
 def extract_data_argparse(args):
     if args.path is None or not os.path.exists(args.path):
-        print('Enter a valid path to a source code"...')
+        print('"%s" is not a valid path, please enter a path to a source code"...' % args.path)
         sys.exit(0)
 
     extract_data(file_path=args.path)
--- utils/misc.py
+++ utils/misc.py
@@ -1,3 +1,7 @@
+import os
+import re
+
+
 def indent(text, spaces=4):
     spaces = ' ' * spaces
     return '\n'.join(spaces + line if line.strip() != '' else line for line in text.split('\n'))
@@ -8,3 +12,11 @@ def listget(li, index, fallback=None):
         return li[index]
     except IndexError:
         return fallback
+
+
+def traverse(path, regex, callback):
+    regex = re.compile(regex)
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f_name in filter(regex.search, filenames):
+            f_path = os.path.join(dirpath, f_name)
+            callback(f_path)
