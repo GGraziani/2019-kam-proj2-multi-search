a431c2eda6787b13ef62a662091be067b39f1c52
gus <gustavo.graz@outlook.com>
2020-01-10 04:21:05 +0100

    Add python extractor

--- extraction/data_extractor.py
+++ extraction/data_extractor.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+from extraction.extractor import PyExtractor
 from utils.misc import traverse
 
 py_files = []
@@ -8,10 +9,13 @@ clang_files = []
 
 
 def extract_data(file_path):
+    print('\n> Extracting names of classes, function and methods (for .cc and .py files) for source "%s"... ' %
+          os.path.basename(file_path))
+
     traverse(file_path, r'\.py$|\.cc$', collect_files)
 
-    print(py_files)
-    print(clang_files)
+    py_extractor = PyExtractor(py_files)
+    print(py_extractor)
 
 
 def collect_files(file):
--- extraction/extractor.py
+++ extraction/extractor.py
@@ -1,13 +1,14 @@
+import ast
+
 import pandas as pd
 
 
-class Extractor:
+class Extractor(object):
     def __init__(self, files):
-        self.m_names = 0
-        self.c_names = 0
-        self.f_names = 0
+        self._functions = 0
+        self._classes = 0
+        self._methods = 0
 
-        self.files = files
         self._row_list = []
         for file in files:
             self._get_data(file)
@@ -22,5 +23,35 @@ class Extractor:
         return pd.DataFrame(self._row_list)
 
     def __str__(self):
-        print(' - functions = "%s"\n - classes = "%s"\n - methods = "%s"\n' %
-              (str(self.f_names), str(self.c_names), str(self.m_names)))
+        return '\n\t- functions: ' + str(self._functions) + \
+               '\n\t- classes: ' + str(self._classes) + \
+               '\n\t- methods: ' + str(self._methods)
+
+
+class PyExtractor(Extractor):
+
+    def _get_data(self, f_path):
+        with open(f_path) as file:
+            node = ast.parse(file.read())
+
+        entities = [e for e in node.body if (isinstance(e, ast.FunctionDef) | isinstance(e, ast.ClassDef))]
+
+        m_count = 0
+        for entity in entities:
+            self._add_entity(entity.name, f_path, entity.lineno)
+
+            if isinstance(entity, ast.ClassDef):
+                self._classes += 1
+                methods = [m for m in entity.body if isinstance(m, ast.FunctionDef)]
+
+                for method in methods:
+                    self._add_entity(method.name, f_path, method.lineno)
+                    m_count += 1
+            else:
+                self._functions += 1
+        self._methods += m_count
+
+    def __str__(self):
+        stats = super().__str__()
+        return '\tPython extractor found:' + stats
+
