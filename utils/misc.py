a2d21c6b4f7c1e11905b17bbfd913d7bc9e12050
gus <gustavo.graz@outlook.com>
2020-01-11 14:52:16 +0100

    Move split_name and add get_top_five based on similarity

--- utils/misc.py
+++ utils/misc.py
@@ -1,9 +1,11 @@
 import os
 import re
 from collections import defaultdict
+from re import finditer
 
 import gensim
 from gensim import models
+from gensim.parsing.preprocessing import STOPWORDS
 
 
 def indent(text, spaces=4):
@@ -29,6 +31,19 @@ def traverse(path, regex, callback):
 # ------- TRAINING ------- #
 
 
+def split_name(name):
+    words = []
+    if isinstance(name, str):  # ignore random "nan" values
+        matches = finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|.*_.*|$)', name)
+
+        for match in matches:
+            words = [match.group(0)]
+            if words[0].__contains__('_'):
+                words = words[0].split("_")
+
+    return [word.lower() for word in words if word != '' and word not in STOPWORDS]
+
+
 def get_not_unique_words(words):
     frequency = defaultdict(int)
     for text in words:
@@ -41,3 +56,18 @@ def w_2_tagged_doc(words):
     for i, line in enumerate(words):
         line = " ".join(line)
         yield models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
+
+
+# ------- QUERY ------- #
+
+
+def get_top_five(df, similarity):
+    c = 0
+    top_five = []
+    for doc_number, score in sorted(enumerate(similarity), key=lambda x: x[1], reverse=True):
+        if c >= 5:
+            break
+        else:
+            top_five.append(df.iloc[doc_number].values.tolist())
+            c += 1
+    return top_five
