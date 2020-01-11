a5708a79e438626a455f6a89d83d7f75f8589734
gus <gustavo.graz@outlook.com>
2020-01-11 20:08:49 +0100

    Add print query

--- training/search_engine.py
+++ training/search_engine.py
@@ -8,16 +8,34 @@ class SearchEngine:
     def __init__(self, data):
         print('\n> Initializing the search engine... ')
         self.corpus = Corpus(df=data)
+        self.query_results = {}
 
     def query(self, query):
         print('\n> Query: "%s"... ' % query)
-        results = {}
         words = query.lower().split()
 
-        results['freq'] = self.corpus.freq(words)
-        results['tf-idf'] = self.corpus.tf_idf(words)
-        results['lsi'] = self.corpus.lsi(words)
-        results['doc2v'] = self.corpus.doc2v(words)
+        self.query_results['freq'] = self.corpus.freq(words)
+        self.query_results['tf-idf'] = self.corpus.tf_idf(words)
+        self.query_results['lsi'] = self.corpus.lsi(words)
+        self.query_results['doc2v'] = self.corpus.doc2v(words)
+
+        self.print_query_results()
+
+    def print_query_results(self):
+        if len(self.query_results) > 0:
+
+            print('\t\t 1. Freq results...')
+            for res in self.query_results['freq']:
+                print('\t\t', res)
+            print('\t\t 2. Tf-idf results...')
+            for res in self.query_results['tf-idf']:
+                print('\t\t', res)
+            print('\t\t 3. LSI results...')
+            for res in self.query_results['lsi']:
+                print('\t\t', res)
+            print('\t\t 4. Doc2V results...')
+            for res in self.query_results['doc2v']:
+                print('\t\t', res)
 
 
 class Corpus:
