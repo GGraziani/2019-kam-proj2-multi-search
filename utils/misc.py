2dcedb5de77821fb9af3e3091e2ebecfee2958b7
gus <gustavo.graz@outlook.com>
2020-01-11 03:33:02 +0100

    Add training related utility functions

--- utils/misc.py
+++ utils/misc.py
@@ -1,5 +1,9 @@
 import os
 import re
+from collections import defaultdict
+
+import gensim
+from gensim import models
 
 
 def indent(text, spaces=4):
@@ -7,7 +11,7 @@ def indent(text, spaces=4):
     return '\n'.join(spaces + line if line.strip() != '' else line for line in text.split('\n'))
 
 
-def listget(li, index, fallback=None):
+def list_get(li, index, fallback=None):
     try:
         return li[index]
     except IndexError:
@@ -20,3 +24,20 @@ def traverse(path, regex, callback):
         for f_name in filter(regex.search, filenames):
             f_path = os.path.join(dirpath, f_name)
             callback(f_path)
+
+
+# ------- TRAINING ------- #
+
+
+def get_not_unique_words(words):
+    frequency = defaultdict(int)
+    for text in words:
+        for token in text:
+            frequency[token] += 1
+    return [[token for token in text if frequency[token] > 1] for text in words]
+
+
+def w_2_tagged_doc(words):
+    for i, line in enumerate(words):
+        line = " ".join(line)
+        yield models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
