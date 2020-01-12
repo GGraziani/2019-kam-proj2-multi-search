88dc25bce384168b383faa5db6de711c9f24cf17
gus <gustavo.graz@outlook.com>
2020-01-12 06:54:08 +0100

    Edit search data argparse: check if query is valid

--- training/search_data.py
+++ training/search_data.py
@@ -15,5 +15,8 @@ def search_data_argparse(args):
     if args.data is None or not (os.path.exists(args.data) and os.path.isfile(args.data)):
         print('"%s" is not a valid path, please enter a path to your "data.csv"...' % args.path)
         sys.exit(0)
+    if args.query is None or args.query == '':
+        print('Query is empty...')
+        sys.exit(0)
 
     search_data(data=pd.read_csv(args.data), query=args.query)
