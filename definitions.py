2b722cff6d8287d7d4733b7f8bbdf73ff9713485
gus <gustavo.graz@outlook.com>
2020-01-13 00:16:18 +0100

    Add lib path

--- definitions.py
+++ definitions.py
@@ -1,6 +1,7 @@
 import os
 
 PROJ_ROOT = os.path.dirname(os.path.abspath(__file__))   # Project root path
+LIB_PATH = os.path.join(PROJ_ROOT, 'lib')
 
 TENSORFLOW_PATH = os.path.join(PROJ_ROOT, 'lib/tensorflow')
 EXTRACTED_DATA_PATH = os.path.join(PROJ_ROOT, 'res/data.csv')
