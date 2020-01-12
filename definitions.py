0991c6558e197b5153a39beda9269c914f10935f
gus <gustavo.graz@outlook.com>
2020-01-12 07:52:22 +0100

    Correct wrong path

--- definitions.py
+++ definitions.py
@@ -4,4 +4,4 @@ PROJ_ROOT = os.path.dirname(os.path.abspath(__file__))   # Project root path
 
 TENSORFLOW_PATH = os.path.join(PROJ_ROOT, 'lib/tensorflow')
 EXTRACTED_DATA_PATH = os.path.join(PROJ_ROOT, 'res/data.csv')
-GROUND_TRUTH_PATH = os.path.join(PROJ_ROOT, 'res/gt.txt')
+GROUND_TRUTH_PATH = os.path.join(PROJ_ROOT, 'res/ground-truth.txt')
